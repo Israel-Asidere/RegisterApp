5284f18a2d6b25a26b767f0abec4fad752d37afa
Israel Asidere <Israel-Asidere@users.noreply.github.com>
2019-08-22 19:45:50 +0100

    My first commit

---
+++ RegisterApp/Controllers/LoginController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RegisterApp.Controllers
+{
+    public class LoginController : Controller
+    { RegisterContext db = new RegisterContext();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //for 'add user' 'new user'
+        public ActionResult NewUser(Owner owner)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Owners.Add(owner);
+                db.SaveChanges();
+                return RedirectToAction("Index", "Login");
+            }
+
+            return View(owner);
+
+        }
+
+        public ActionResult Check()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Authorize(RegisterApp.Owner userModel)
+        {
+            using (RegisterContext db = new RegisterContext())
+            {
+                Owner userDetails = db.Owners.Where(x => x.UserName == userModel.UserName && x.Password == userModel.Password).FirstOrDefault();
+                if (userDetails == null)
+                {
+                    userModel.LoginErrorMessage = "Wrong Username or Password";
+                }
+                else
+                {
+                    Session["OwnerID"] = userDetails.OwnerID;
+                    Session["OwnerName"] = userDetails.UserName;
+                    Owner usrDetails = db.Owners.Where(x => x.OwnerID == userModel.OwnerID).FirstOrDefault();
+                    if (usrDetails == null)
+                    {
+                        //return new instance of the Index view
+                        ViewBag.userName = userDetails.UserName;
+                        return RedirectToAction("Index", "User");
+
+                    }
+                    else
+                    {
+                        return View(db.Users.ToList());
+                    }
+
+                }
+            }
+            return View("Index", userModel);
+        }
+    }
+}
---
+++ RegisterApp/Controllers/UserController.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace RegisterApp.Controllers
+{
+
+    public class UserController : Controller
+    {
+        public ActionResult AddOrEdit()
+        {
+            User usermodel = new User();
+            using (RegisterContext db = new RegisterContext())
+            {
+                usermodel.PositionCollection = db.Pos.ToList<Pos>();
+            }
+            return View(  usermodel);
+        }
+        [HttpPost]
+        public ActionResult AddOrEdit(User user)
+        {
+            if (ModelState.IsValid)
+            {
+
+                db.Users.Add(user);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+        RegisterContext db = new RegisterContext();
+        public ActionResult Index(User userModel)
+        {
+            
+                return View(db.Users.ToList());   
+        }
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult Details(int? id)
+        {
+
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            User user = db.Users.Find(id);
+            if (user == null)
+                return HttpNotFound();
+            return View(user);
+        }
+        [HttpGet]
+        public ActionResult Edit([Bind(Include = "UserName, UserPosID, IsUserPresent,Date,TimeI,TimeOut")]int? id)
+        {
+            RegisterContext db = new RegisterContext();
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            User user = db.Users.Single(reg => reg.UserID == id);
+            if (user == null)
+                return HttpNotFound();
+            using (db)
+            {
+                user.PositionCollection = db.Pos.ToList<Pos>();
+            }
+            return View(user);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(user).State = EntityState.Modified;
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View(user);
+        }
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            User user = db.Users.Find(id);
+            if (user == null)
+                return HttpNotFound();
+            return View(user);
+        }
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(FormCollection formCollection, int id)
+        {
+            User user = db.Users.Find(id);
+            db.Users.Remove(user);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult OrderByDate()
+        {
+            var user = from p in db.Users
+                       orderby p.Date
+                       select p;
+            return View(user);
+
+        }
+        public ActionResult OrderByName()
+        {
+            var user = from p in db.Users
+                       orderby p.UserName
+                       select p;
+            return View(user);
+
+        }
+        public ActionResult OrderByPresent()
+        {
+            var user = from p in db.Users
+                       orderby p.IsUserPresent
+                       select p;
+            return View(user);
+
+        }
+    }
+}
